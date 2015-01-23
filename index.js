9bddce1a51c66e5c0301f1c5752fbb4d03d0dc6e
lenin.anduin <leninsuro@gmail.com>
2015-01-22 20:35:05 -0600

    postmark inbound + store in db

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express')
 var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var moment = require('moment');
+var bankParser = require('./lib/bank-parser');
 
 
 app.set('port', (process.env.PORT || 5000))
@@ -18,22 +20,25 @@ app.post('/inbound', function(request, response){
     if (!request.body) {
         return response.sendStatus(400)
     }
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        if (err) {
-            console.error(err); response.send("Error " + err);
-        }
-        if (client) {
+    var opv = bankParser.getOperatonValues(request.body, moment);
 
-            client.query('INSERT into cash_operations (from_e, text, date) VALUES($1, $2, $3)',[request.body.From, request.body, request.body.Date ] ,
-                function(err, result) {
-                done();
-                if (err)
-                { console.error(err); response.send("Error " + err); }
-            });
-        }
-    });
+    if (!opv.error){
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+            if (err) {
+                console.error(err); response.send("Error " + err);
+            }
+            if (client) {
+                client.query('INSERT into cash_operations (user_email, ammount, type, auth_num, date, bank ) VALUES($1, $2, $3, $4, $5, $6)',[request.body.From, opv.ammount, opv.type, opv.auth_num, opv.date, opv.bank  ] ,
+                    function(err, result) {
+                    done();
+                    if (err)
+                    { console.error(err); response.send("Error " + err); }
+                });
+            }
+        });
+    }
 
-    response.json(request.body.StrippedTextReply);
+    response.json(opv);
 });
 
 app.listen(app.get('port'), function() {
