afec38cf79d99bb2e0966ace96f02074b51d2de8
lenin.anduin <leninsuro@gmail.com>
2015-01-22 18:14:45 -0600

    postmark test

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ var express = require('express')
 var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
+var bodyParser = require('body-parser')
+
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
     var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-        result += cool();
-    response.send(result);
+    response.send(cool());
 });
 
 app.get('/db', function (request, response) {
@@ -19,7 +18,7 @@ app.get('/db', function (request, response) {
             console.error(err); response.send("Error " + err);
         }
         if (client) {
-            client.query('SELECT * FROM test_table', function(err, result) {
+            client.query('SELECT * FROM cash_operations', function(err, result) {
                 done();
                 if (err)
                 { console.error(err); response.send("Error " + err); }
@@ -30,9 +29,28 @@ app.get('/db', function (request, response) {
     });
 });
 
+// parse application/json
+app.use(bodyParser.json())
 app.post('/inbound', function(request, response){
-    console.log(request.body);
-    response.end("yes");
+    if (!request.body) {
+        return response.sendStatus(400)
+    }
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            console.error(err); response.send("Error " + err);
+        }
+        if (client) {
+
+            client.query('INSERT into cash_operations (from_e, text, date) VALUES($1, $2, $3)',[request.body.From, request.body.StrippedTextReply, request.body.Date ] ,
+                function(err, result) {
+                done();
+                if (err)
+                { console.error(err); response.send("Error " + err); }
+            });
+        }
+    });
+
+    response.json(request.body.StrippedTextReply);
 });
 
 app.listen(app.get('port'), function() {
